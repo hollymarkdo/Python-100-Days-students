efe03340d77ca46bfbfeaadcde15680274de82b0
hollymarkdo <43453176+hollymarkdo@users.noreply.github.com>
2019-06-10 02:38:49 +0800

    Add files via upload

--- mytest.py
+++ mytest.py
@@ -1,5 +1,19 @@
-import turtle
-turtle.pencolor("blue")
-for i in range(3):
-    turtle.forward(100)
-    turtle.left(120)
+if __name__ == '__main__':
+#experiment1
+    import math
+
+    radius =float(input("please input the circle's radius r = "))
+    area =math.pi*radius*radius
+    h = float(input("please input the hight h = "))
+    print("the volume is ",area*h)
+
+#experiment2
+    num = int(input("please input a num to judge if it is a prime number num = "))
+    flag = int(0)
+    for i in range(2,num-1):
+        if(num%i!=0):
+            flag=1
+    if(flag==1):
+        print("this num is prime number")
+    else:
+        print("this num isn't prime number")
